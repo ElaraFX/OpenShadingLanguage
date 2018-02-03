b1ad9b434817d3a36a1fa93312e078d13dc49da4
Elvic <43492276@qq.com>
2018-02-03 17:13:26 +0800

    Added negative op.

--- src/liboslexec/backendglsl.cpp
+++ src/liboslexec/backendglsl.cpp
@@ -25,6 +25,7 @@ static ustring op_return("return");
 static ustring op_exit("exit");
 static ustring op_useparam("useparam");
 static ustring op_assign("assign");
+static ustring op_neg("neg");
 static ustring op_add("add");
 static ustring op_sub("sub");
 static ustring op_mul("mul");
@@ -535,7 +536,9 @@ bool BackendGLSL::build_op(int opnum)
 
 		return true;
 	}
-	else if (op.opname() == op_assign)
+	else if (
+		op.opname() == op_assign || 
+		op.opname() == op_neg)
 	{
 		Symbol & result = *opargsym(op, 0);
 		Symbol & src = *opargsym(op, 1);
@@ -543,6 +546,9 @@ bool BackendGLSL::build_op(int opnum)
 		begin_code("");
 		gen_symbol(result);
 		add_code(" = ");
+		if (op.opname() == op_neg) {
+			add_code("- ");
+		}
 		gen_symbol(src);
 		add_code(";\n");
 
